5b2c345c68e11a120bee6a24277ea0688b254a60
Lars Kotthoff <larsko@cs.ubc.ca>
2015-10-16 14:47:48 -0700

    more meaningful testing

--- test/java/weka/classifiers/meta/AutoWEKAClassifierTest.java
+++ test/java/weka/classifiers/meta/AutoWEKAClassifierTest.java
@@ -1,14 +1,20 @@
 package weka.classifiers.meta;
 
-import weka.classifiers.AbstractClassifier;
+import weka.classifiers.Evaluation;
 import weka.classifiers.meta.AutoWEKAClassifier;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AutoWEKAClassifierTest {
     @Test
-    public void testClassifier() {
+    public void testClassifier() throws Exception {
         String[] args = {"-t", "test/iris.arff", "-no-cv", "-timeLimit", "1"};
-        AbstractClassifier.runClassifier(new AutoWEKAClassifier(), args);
+        String out = Evaluation.evaluateModel(new AutoWEKAClassifier(), args);
+
+        assertNotNull(out);
+        assertTrue(out.indexOf("classifier: weka.classifiers.") > -1);
+        assertTrue(out.indexOf("Error on training data") > -1);
+        assertTrue(out.indexOf("Total Number of Instances              150") > -1);
     }
 }
