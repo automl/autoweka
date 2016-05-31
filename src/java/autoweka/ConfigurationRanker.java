18aa2d1271244a23014dcc4ebfe97af3b05ded7f
Oto Alves <otopalves@gmail.com>
2016-05-31 12:11:27 -0700

    maybe i messed things up. Will try to reset

--- src/java/autoweka/ConfigurationRanker.java
+++ src/java/autoweka/ConfigurationRanker.java
@@ -11,53 +11,54 @@ import java.io.File;
 
 
 public class ConfigurationRanker{
-	
+
 	/*
 		Static part for ranking configurations in the end of the run
 	*/
-	
+
 	//Loads configurations from temporary log, merges identical while merging the folds in which they were analyzed, sorts them and spits n of them to a xml
 	public static void rank(int n, String aTemporaryLogPath,String aSortedLogPath){
-		
-		initializeLog(aSortedLogPath);
+
+		//initializeLog(aSortedLogPath);
 
 		HashMap<Integer,Configuration> configurationsMap = mergeConfigurations(aTemporaryLogPath);
-		
+
 		List<Configuration> mergedConfigurations = new ArrayList<Configuration>(configurationsMap.values());
 		Collections.sort(mergedConfigurations);
-		
+		Collections.reverse(mergedConfigurations);
+
 		ArrayList<Configuration> choppedList = new ArrayList<Configuration>();
 		int limit = (n<mergedConfigurations.size())?(n):(mergedConfigurations.size());
-		for(int i=0; i<limit ; i++){
+		for(int i=0; i<limit ; i++){ //Starting from the end because sort() sorts from worst to best. @TODO check if theres a standard method for chopping
 			choppedList.add(mergedConfigurations.get(i));
 		}
-		
+
 		ConfigurationCollection spit = new ConfigurationCollection(choppedList);
 
 		spit.toXML(aSortedLogPath);// ba dum tss
 	}
-	
+
 	//Merging equivalent configurations while keeping track of the folds in which each instance was analyzed
 	private static HashMap<Integer,Configuration> mergeConfigurations(String aTemporaryLogPath){
-		
+
 		HashMap<Integer,Configuration> configurationsMap = new HashMap<Integer,Configuration>();
 		ConfigurationCollection loadedCC = ConfigurationCollection.fromXML(aTemporaryLogPath,ConfigurationCollection.class);
 		ArrayList<Configuration> configurationsArrayList= loadedCC.asArrayList();
-		
-		int ciFoldId;
+
 		Integer ciKey;
-		double ciScore;
-			
+
 		for(Configuration ciConfig : configurationsArrayList){
-			ciFoldId = ciConfig.getEvaluatedFold();
-			ciKey   = ciConfig.hashCode();
-			ciScore = ciConfig.getScore();
+
+			ciKey = ciConfig.hashCode();
+
 			if(configurationsMap.containsKey(ciKey)){
-				configurationsMap.get(ciKey).updateEvaluationCollection( ciScore,ciFoldId );
+				System.out.println("merging! "+ciConfig.hashCode());
+				configurationsMap.get(ciKey).mergeWith(ciConfig);
 			}else{
+				System.out.println("putting! "+ciConfig.hashCode());
 				configurationsMap.put(new Integer(ciKey),ciConfig);
-				configurationsMap.get(ciKey).updateEvaluationCollection( ciScore,ciFoldId );
 			}
+
 		}
 
 		return configurationsMap;
@@ -68,7 +69,14 @@ public class ConfigurationRanker{
 
 		try{
 			File logFile = new File(aLogPath);
-			logFile.createNewFile();
+			if(!logFile.exists()){
+				logFile.createNewFile();
+				ConfigurationCollection empty = new ConfigurationCollection();
+				empty.toXML(aLogPath);
+				System.out.println("HUE "+aLogPath);
+			}
+
+			System.out.println("HUE HUE"+aLogPath);
 		}catch(Exception e){
 			System.out.println("ConfigurationCollection: Couldn't make log file out of xml path!");
 		}
