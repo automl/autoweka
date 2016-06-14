843ff3c02f526f4ba011e06cb2a42f4264952576
Oto Alves <otopalves@gmail.com>
2016-06-14 15:35:02 -0700

    Removed pointless imports

--- src/java/autoweka/ConfigurationRanker.java
+++ src/java/autoweka/ConfigurationRanker.java
@@ -5,9 +5,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-import weka.classifiers.meta.AutoWEKAClassifier;
-
-import java.io.File;
 
 
 
