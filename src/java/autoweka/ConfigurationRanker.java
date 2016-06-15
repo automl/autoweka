3c37332b256d9034b1fbd71833d84874a23d3dc9
Oto Alves <otopalves@gmail.com>
2016-06-15 13:37:07 -0700

    Fixed an issue with the configuration ranker and its unit test

--- src/java/autoweka/ConfigurationRanker.java
+++ src/java/autoweka/ConfigurationRanker.java
@@ -16,14 +16,13 @@ public class ConfigurationRanker{
 
 		List<Configuration> mergedConfigurations = mergeConfigurations(aTemporaryLogPath);
 
-
 		ArrayList<Configuration> choppedList = chopList(mergedConfigurations,n);
 
-		forceFirst(choppedList,smacBest);
-
+		if(!smacBest.equals("IGNORE")){ //We might want to ignore this just for testing purposes. @TODO find a more elegant way to test
+			forceFirst(choppedList,smacBest);
+		}
 
 		ConfigurationCollection spit = new ConfigurationCollection(choppedList);
-
 
 		spit.toXML(aSortedLogPath);// ba dum tss
 	}
