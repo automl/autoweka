0689e47e00f987b37ba5d3e3417bcad3a9dddfc4
Oto Alves <otopalves@gmail.com>
2016-06-15 18:20:03 -0700

    Stupid import

--- test/java/autoweka/AutoWEKATestSuite.java
+++ test/java/autoweka/AutoWEKATestSuite.java
@@ -4,6 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import autoweka.instancegenerators.*;
 
+import autoweka.smac.SMACTrajectoryParserTester;
+
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     UtilTester.class,
