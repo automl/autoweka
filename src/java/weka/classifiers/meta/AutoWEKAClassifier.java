b4e91bc1d5ae5b33bf21358a1c7e38eb9c7c615d
otoalves <otopalves@gmail.com>
2016-09-14 20:03:16 -0300

    -

--- src/java/weka/classifiers/meta/AutoWEKAClassifier.java
+++ src/java/weka/classifiers/meta/AutoWEKAClassifier.java
@@ -461,7 +461,7 @@ public class AutoWEKAClassifier extends AbstractClassifier implements Additional
        eval.evaluateModel(classifier, is);
      }catch(NullPointerException e){
        System.out.println("---\n\n Auto-WEKA couldn't find any usable configuration within the alloted time.\n Please give Auto-WEKA more time.");
-       System.exit();//throw new NotEnoughTimeException();
+       System.exit(1);//throw new NotEnoughTimeException();
      }
 
 
